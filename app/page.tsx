b57420d524d0c017d042cd6d9ba22ddfa23f644c
TrakaDev <trakadev@gmail.com>
2023-06-21 19:21:46 -0600

    add mainTitle font

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,41 @@
 "use client"
 import { Loader } from "./components/Loader"
 import { NavBar } from "./components/NavBar"
-import { Image, Flex } from "@chakra-ui/react"
+import { Image, Flex, Heading } from "@chakra-ui/react"
 
 export default function Home() {
   return (
     <main>
       <Loader />
       <NavBar />
-      <Flex h={"90vh"} w={"100vw"} justify="center" align="center">
-        <Image src="ape-juice.png" alt="ape juice" h={"90vh"} />
+      <Flex
+        h={"90vh"}
+        w={"100vw"}
+        justify={{ base: "start", md: "space-around" }}
+        align="center"
+        flexDir={{ base: "column-reverse", md: "row" }}
+      >
+        <Image
+          src="ape-juice.png"
+          alt="ape juice"
+          w={{ base: "100%", md: "xl" }}
+        />
+        <Flex flexDir={"column"} align={"center"} justify={"center"} p={5}>
+          <Heading
+            textAlign={"center"}
+            fontFamily={"mainTitle"}
+            fontSize={{ base: "5xl", md: "4xl", lg: "7xl" }}
+          >
+            MEME JUICE
+          </Heading>
+          <Heading
+            textAlign={"center"}
+            fontFamily={"mainTitle"}
+            fontSize={{ base: "4xl", md: "3xl", lg: "5xl" }}
+          >
+            Taste the future with us!
+          </Heading>
+        </Flex>
       </Flex>
     </main>
   )
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,18 @@
 "use client"
 
 import { CacheProvider } from "@chakra-ui/next-js"
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  fonts: {
+    mainTitle: "Lobster, cursive",
+  },
+})
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
-      <ChakraProvider cssVarsRoot={undefined}>{children}</ChakraProvider>
+      <ChakraProvider theme={theme} cssVarsRoot={undefined}>{children}</ChakraProvider>
     </CacheProvider>
   )
 }
